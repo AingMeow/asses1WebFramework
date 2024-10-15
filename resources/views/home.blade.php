bc726cb0fb11a333a8158c861f7a939ad6ff0f90
iputraseno <ivanputraseno010202@gmail.com>
2024-10-15 23:23:17 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,8 +3,9 @@
 
 @section('content')
 <div class="container">
-    <h1>Welcome to My Laravel Application</h1>
-    <p>This is the home page.</p>
+    <h1>Selamat Datang di Sitter Hub </h1>
+    <p>Tempat terbaik untuk menemukan pengasuh yang dapat diandalkan dan profesional. Kami hadir untuk membantu Anda menemukan layanan pengasuhan terbaik yang sesuai dengan kebutuhan keluarga Anda. Dengan pengasuh yang terverifikasi dan berpengalaman, Sitter Hub memberikan ketenangan pikiran saat Anda meninggalkan orang-orang terkasih di tangan yang tepat.</p>
+    <p>Jelajahi layanan kami dan temukan solusi pengasuhan yang aman, fleksibel, dan terpercaya.</p>
     <a href="#" class="btn btn-primary">Click Me!</a>
 </div>
 @endsection
